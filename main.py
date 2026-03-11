7cf86b1533f171815b5e916731ee2afdc13d9714
tamar katz <tamarkatz@h-MacBook-Pro-sl-tamar.local>
2026-03-11 15:21:36 -0700

    Use os.environ.get PORT for Railway

--- main.py
+++ main.py
@@ -144,5 +144,5 @@ def download_weekly_report():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
